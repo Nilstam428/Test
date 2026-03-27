2cebd32badd585c5955674d4ce2f6f560ccade1f
Nilesh <nileshtamboli428@gmail.com.com>
2026-03-28 01:43:39 +0530

    Update.

--- Medium/04_overload.py
+++ Medium/04_overload.py
@@ -1,13 +1,29 @@
 class Number:
-  def add(self,a,b):
-    result = a+b
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
+    print("Constructor function")
+
+
+  def add(self):
+    result = self.a + self.b 
     return result
   #function overloading 
   # Implementing same function with different functionality
-  def add(self, a, b):
-    res = a*b
+  def mul(self):
+    res = self.a * self.b 
     return res
   
+class Digit(Number):
+  def __init__(self,a,b):
+    super().__init__(a, b)
+
+  def mul(self):
+    print("Calling from from child class")
+    return self.a * self.b 
+   
   
-obj = Number()    
-print(obj.add(3,4))
+obj = Number(4,5)
+obj1 = Digit(2,2)
+print(obj.mul())
+print(obj1.mul())
--- Medium/my_module.py
+++ Medium/my_module.py
@@ -6,3 +6,5 @@ def sub(a,b):
   result = a-b
   return result
 
+print(add(5,3))
+print(sub(5,3))
